b681d29e58b7ddf844f04ec60abcc8f36e70f4ac
1F10210096 <koki.1218.dog@icloud.com>
2023-05-16 00:34:10 +0900

    ゲーム性は完成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,35 +17,50 @@ const Home = () => {
     console.log(x, y);
     const newBoard: number[][] = JSON.parse(JSON.stringify(board));
     const box = [];
-    if (newBoard[y][x] === 0) {
+    if (board[y][x] === 0) {
+      console.log('a');
       for (let i = -1; i <= 1; i++) {
         for (let j = -1; j <= 1; j++) {
           let y_i = y + i;
           let x_j = x + j;
-          if (
-            newBoard[y_i]?.[x_j] !== undefined &&
-            newBoard[y_i][x_j] !== 0 &&
-            newBoard[y_i][x_j] !== turnColor
-          ) {
-            while (newBoard[y_i]?.[x_j] !== undefined && newBoard[y_i][x_j] !== turnColor) {
-              if (newBoard[y_i]?.[x_j] !== undefined && newBoard[y_i][x_j] === 0) {
+          {
+            console.log('b');
+            for (let g = 0; g < 7; g++) {
+              console.log('aaaaa');
+              if (board[y_i] === undefined || board[x_j] === undefined || board[y_i][x_j] === 0) {
+                console.log('c');
                 box.length = 0;
                 break;
+              } else if (board[y_i][x_j] === turnColor) {
+                console.log('c');
+                break;
+              } else {
+                console.log('d');
+                box.push([y_i, x_j]);
+                y_i += i;
+                x_j += j;
               }
-              box.push([y_i, x_j]);
-              y_i += i;
-              x_j += j;
-              newBoard[y][x] = turnColor;
             }
-            if (box.length > 0) { 
+            if (box.length > 0) {
               box.forEach(([y_i, x_j]) => {
                 newBoard[y_i][x_j] = turnColor;
               });
+
+              console.log('e');
+            }
+            if (box.length > 0) {
+              box.forEach(([y2, x2]) => {
+                newBoard[y2][x2] = turnColor;
+              });
+              newBoard[y][x] = turnColor;
+              console.log(y, x);
+              console.log('f');
             }
           }
-          if (box.length > 0) { 
+          if (box.length > 0) {
             setBoard(newBoard);
             setTurnColor(2 / turnColor);
+            console.log('g');
           }
         }
       }
@@ -53,6 +68,10 @@ const Home = () => {
   };
   return (
     <div className={styles.container}>
+      <div className={styles.me} />
+      <div className={styles.text1} />
+      <div className={styles.component} />
+
       <div className={styles.board}>
         {board.map((row, y) =>
           row.map((color, x) => (
