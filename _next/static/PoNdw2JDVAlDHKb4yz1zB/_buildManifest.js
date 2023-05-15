6a4eb1bf7018613390a66ed8cbbfb3822eb64d1e
1F10210096 <1F10210096@users.noreply.github.com>
2023-05-15 01:46:06 +0000

    deploy: 38cff9c36af052d267346ee5ba0ba7d39a4052c4

---
+++ _next/static/PoNdw2JDVAlDHKb4yz1zB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/5e3044c1fe69671e.css","static/chunks/pages/index-5eee9435e1d50ff1.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
