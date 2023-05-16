adb449557e4469dc87995ce13d78e56ff861281e
1F10210096 <1F10210096@users.noreply.github.com>
2023-05-16 15:00:42 +0000

    deploy: 60cb5704188dd76d4beb5344704ebc2776657af9

---
+++ _next/static/ZM1pYOeixFf3zqbi07IeG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/4695aed6cc3f5a16.css","static/chunks/pages/index-a20a7de25256c860.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
